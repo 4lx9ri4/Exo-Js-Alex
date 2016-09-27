bae1c9cd11bb09985d2b34e059868bb4836e616a
4lx9ri4 <tuil_alexandre@hotmail.com>
2016-09-27 14:35:47 +0200

    Exo terminé merci flo

---
+++ script.js
@@ -0,0 +1,10 @@
+function changerStyle(){
+  /*Modifi la couleur de parag1*/
+        document.getElementById('parag1').style.color="#FFF";
+  /*Modifi la couleur du background de body*/
+        document.body.style.backgroundColor="#000";
+  /*Modifi les bordures de parag1*/
+        document.getElementById('parag1').style.border="10px dotted red";
+  /*Modifi les marges interne de parag*/
+        document.getElementById('parag1').style.padding="5px";
+}
