453c4f067e40ab45c470333f01ea5a235ff57e99
4lx9ri4 <tuil_alexandre@hotmail.com>
2016-09-27 16:31:10 +0200

    à modifier

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ function changerStyle(){
   /*Modifi les marges interne de parag*/
         document.getElementById('parag1').style.padding="5px";
 }
+/*générer un paragraphe via un bouton*/
+function afficher_cacher(id)
+{
+    if(document.getElementById(id).style.visibility=="hidden")
+    {
+        document.getElementById(id).style.visibility="visible";
+        document.getElementById('bouton_'+id).innerHTML='Cacher le texte';
+    }
+    else
+    {
+        document.getElementById(id).style.visibility="hidden";
+        document.getElementById('bouton_'+id).innerHTML='Afficher le texte';
+    }
+    return true;
+}
